4382b04caf7bc31a8b449b2e6ee0c30aeff72d28
Fernando Bohlhalter <fernandobohlhalter@gmail.com>
2024-06-17 15:11:36 -0300

    text color change

--- center_func.py
+++ center_func.py
@@ -1,6 +1,7 @@
+# Centralize the window on the middle of screen
 def center_screen(root, app_width, app_height):
-    screen_wid = root.winfo_screenwidth()
-    screen_hei = root.winfo_screenheight()
+    screen_wid = root.winfo_screenwidth() # gets screen res x
+    screen_hei = root.winfo_screenheight() #gets screen res y
     x_pos = int(screen_wid/2)
     y_pos = int((screen_hei/2) - (app_height/2))
 
--- yt_download.py
+++ yt_download.py
@@ -42,20 +42,22 @@ text.place(relx=0.5, rely= 0.15, anchor='c')
 
 cframe = ctk.CTkFrame(mainframe, fg_color=black, height=300, width=500)
 cframe.place(relx=0.5, rely=0.63, anchor="c")
-#cframe.pack(fill='x', expand=True)
 
-label1 = ctk.CTkLabel(cframe, text_color=gray, font=('Helvetica', 11), text='Insert link:')
+#Insert link section
+label1 = ctk.CTkLabel(cframe, font=('Helvetica', 11), text='Insert link:')
 label1.place(relx=0.255, rely=0.03, anchor='nw')
 textBox = ctk.CTkEntry(cframe, fg_color='#090909', border_width=1, border_color=dark_gray, width=173)
 textBox.place(relx=0.6, rely=0.12, anchor='ne')
 res_btn = ctk.CTkButton(cframe, text='Ok', fg_color=dark_gray, text_color='white', font=('Helvetica', 12, 'bold'), border_width=0, width=50, hover_color=('#CF0000'), command= set_res_btn)
 res_btn.place(relx=0.65, rely=0.12, anchor='nw')
 
-label2 = ctk.CTkLabel(cframe, text_color=gray, font=('Helvetica', 11), text='Resolution:')
+#Select resolution section
+label2 = ctk.CTkLabel(cframe, font=('Helvetica', 11), text='Resolution:')
 label2.place(relx=0.25, rely=0.30, anchor='nw')
 resBox = ctk.CTkComboBox(cframe, width=250, fg_color='#090909', font=('Helvetica', 13), dropdown_font=('Helvetica', 13), button_color=dark_gray, button_hover_color=('#CF0000'), dropdown_fg_color=dark_gray, dropdown_hover_color=gray,border_width=1, border_color=dark_gray, corner_radius=7, values=placeholder)
 resBox.place(relx=0.5, rely=0.44, anchor='c')
 
+#Download button
 download_btn = ctk.CTkButton(cframe, text='Download', fg_color=dark_gray, text_color='white', font=('Helvetica', 15, 'bold'), border_width=0, height=50, width=120, corner_radius=10, hover_color=('#CF0000'), command=download)
 download_btn.place(relx = 0.5, rely = 0.65, anchor='c')
 #-----------=============App body=============-----------#
